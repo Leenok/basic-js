bd3929d4d3fa51a9eb0272c7168aece5bb8bf7e0
Li <li@gmail.com>
2022-10-18 02:51:28 +0300

    feat: add new solush

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let nstr = "";
+  let n = 1;
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i] != str[i + 1]) {
+      if (n > 1) {
+        nstr += n + str[i];
+      } else {
+        nstr += str[i];
+      }
+      n = 1;
+    } else {
+      n += 1;
+
+    }
+  }
+  return nstr;
 }
 
 module.exports = {
